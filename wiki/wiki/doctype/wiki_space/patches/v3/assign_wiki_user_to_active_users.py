2f2380fbc81e13e50ad8c77aeeb98dd97043b586
Hussain Nagaria <hussainbhaitech@gmail.com>
2026-01-06 12:13:27 +0530

    chore: bring back patch

--- wiki/wiki/doctype/wiki_space/patches/v3/assign_wiki_user_to_active_users.py
+++ wiki/wiki/doctype/wiki_space/patches/v3/assign_wiki_user_to_active_users.py
@@ -4,5 +4,8 @@ import frappe
 def execute():
 	active_users = frappe.db.get_all("User", filters={"enabled": 1}, pluck="name")
 	for user in active_users:
-		frappe.get_doc("User", user).add_roles("Wiki User")
-		frappe.db.commit()
+		try:
+			frappe.get_doc("User", user).add_roles("Wiki User")
+			frappe.db.commit()
+		except Exception:
+			pass
